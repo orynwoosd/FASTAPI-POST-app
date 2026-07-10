a90282cc10215e6a8b3e9ed7b6db9a0f27d59177
orynwoosd <galabeoryn22@gmail.com>
2026-07-10 15:56:36 -0700

    Added env vars to workflow

--- app/database.py
+++ app/database.py
@@ -15,6 +15,7 @@ from .config import settings
 # from .con
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+# SQLALCHEMY_DATABASE_URL = settings.database_url
 engine = create_engine(SQLALCHEMY_DATABASE_URL)  
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -57,8 +57,8 @@ app.add_middleware(
 while True:
     try:
         conn = psycopg2.connect(
-            host="localhost", database=settings.database_name, 
-            user="postgres", password=settings .database_password,
+            host=settings.database_hostname, database=settings.database_name, 
+            user=settings.database_username, password=settings .database_password,
             cursor_factory=RealDictCursor
             )
         cursur = conn.cursor()
@@ -67,7 +67,7 @@ while True:
     except Exception as e:
         time.sleep(2) 
         print(e)
-        print("connection databse failed")
+        print("connection database failed")
 
 
 app.include_router(post.router)
